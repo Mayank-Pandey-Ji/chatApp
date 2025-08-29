3c64a23abd326552155366d08e32626f0d61089d
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-08-29 19:22:21 +0530

    finally completed

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from "react";
-import AuthContext from './AuthContext'
+import { createContext, useContext, useEffect, useState } from "react";
+import {AuthContext} from './AuthContext.jsx'
 import toast from "react-hot-toast";
 
 export const ChatContext = createContext();
@@ -63,10 +63,36 @@ export const ChatProvider = ({ children }) => {
     }
 
     // function to subscribe to messages for selected user
-    const 
+    const subscribeToMessages = async () => {
+        if(!socket) return;
+        socket.on("newMessage",(newMessage)=>{
+            if(selectedUser && newMessage.senderId === selectedUser._id)
+            {
+                newMessage.seen = true;
+                setMessages((prevMessages) => [...prevMessages , newMessage]);
+                axios.put(`/api/messages/mark/${newMessage._id}`)
+            }
+            else 
+            {
+                setUnseenMessages((prevUnseenMessages)=>({
+                    ...prevUnseenMessages ,[newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] +1 : 1
+                }))
+            }
+        })
+    }
+
+    // function to unsubscribe from messages
+    const unsubscribeFromMessages = () => {
+        if(socket) socket.off("newMessage");
+    }
+
+    useEffect(()=>{
+        subscribeToMessages();
+        return ()=> unsubscribeFromMessages();
+    },[socket,selectedUser])
 
     const value = {
-
+        messages,users,selectedUser,getUsers,getMessages,sendMessage,setSelectedUser,unseenMessages,setUnseenMessages
     };
 
     return (
--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -6,7 +6,7 @@ import {io  , userSocketMap} from '../server.js'
 
 export const getUsersForSidebar = async (req , res)=>{
     try {
-        const userId = req.user._id;
+        const userId = req.user._id.toString();
         const filterUsers = await User.find({_id : {$ne:userId}}).select("-password");
 
         // count number of messages not seen 
@@ -35,7 +35,7 @@ export const getUsersForSidebar = async (req , res)=>{
 export const getMessages = async (req , res)=>{
     try{
         const { id: selectedUserId } = req.params;
-        const myId = req.user._id;
+        const myId = req.user._id.toString();
 
         const messages = await Message.find({
             $or: [
@@ -66,7 +66,7 @@ export const markMessageAsSeen  = async (req,res) => {
     }
     catch(error)
     {
-        console.log(error.message);
+        console.log(error.message );
         res.json({success : false , message :error.message})
     }
 }
@@ -96,7 +96,7 @@ export const sendMessage = async (req,res)=>{
         const receiverSocketId = userSocketMap[receiverId];
         if(receiverSocketId)
         {
-            io.to(receiverSocketId).emit("newMessage", newMessage)
+            io.to(receiverSocketId).emit("newMessage", newMessage.toObject ? newMessage.toObject() : newMessage)
         }
 
         res.json({success:true, newMessage})
